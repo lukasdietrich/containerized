3da231ba654aefbee8f91f75148d2cdb52f10fff
Lukas Dietrich <lukas@lukasdietrich.com>
2021-06-30 23:07:17 +0200

    Do not use python >3.5 features

--- update-versions.py
+++ update-versions.py
@@ -17,7 +17,7 @@ class Updater:
     def write_version_file(self, latest_version):
         with open(self.version_file, "r+") as f:
             current = f.read()
-            print(f" -> Current version = {current} (from {self.version_file})")
+            print(" -> Current version = {} (from {})".format(current, self.version_file))
 
             f.seek(0)
             f.write(latest_version)
@@ -26,8 +26,10 @@ class Updater:
             return current != latest_version
 
     def commit_changes(self, latest_version):
+        message = "Update {} to version '{}'".format(self.name, latest_version)
+
         subprocess.run(["git", "add", self.version_file])
-        subprocess.run(["git", "commit", "-m", f"Update {self.name} to version '{latest_version}'"])
+        subprocess.run(["git", "commit", "-m", message])
 
 class NpmUpdater(Updater):
     def __init__(self, name, version_file, npm_package):
@@ -35,18 +37,18 @@ class NpmUpdater(Updater):
         super().__init__(name, version_file)
 
     def update(self):
-        print(f"Updating {self.name} via npm")
+        print("Updating {} via npm".format(self.name))
 
         latest_version = self.fetch_from_registry()
         super().update(latest_version)
 
     def fetch_from_registry(self):
-        url = f"https://registry.npmjs.org/{self.npm_package}"
+        url = "https://registry.npmjs.org/{}".format(self.npm_package)
         res = requests.get(url)
         data = res.json()
         latest = data["dist-tags"]["latest"]
 
-        print(f" -> Latest version = {latest} (from {url})")
+        print(" -> Latest version = {} (from {})".format(latest, url))
         return latest
 
 def main():
