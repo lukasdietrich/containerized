58a709f4343c541945c9f14262897bfe6bc2badd
Lukas Dietrich <lukas@lukasdietrich.com>
2021-07-01 21:52:11 +0200

    Refactor update script

--- update-versions.py
+++ update-versions.py
@@ -4,59 +4,66 @@ import requests
 import subprocess
 
 class Updater:
-    def __init__(self, name, version_file):
+
+    def __init__(self, name, version_file, version_fetcher):
         self.name = name
         self.version_file = version_file
+        self.version_fetcher = version_fetcher
 
-    def update(self, latest_version):
-        updated = self.write_version_file(latest_version)
-        if updated:
-            print(" => Committing changes to git")
+    def update(self):
+        print("=> Updating {}".format(self.name))
+
+        latest_version = self.version_fetcher.fetch_latest()
+        current_version = self.write_version_file(latest_version)
+
+        print("-> Current = {}, Latest = {}".format(current_version, latest_version))
+
+        if latest_version != current_version:
+            print("=> Committing changes to git")
             self.commit_changes(latest_version)
 
     def write_version_file(self, latest_version):
         with open(self.version_file, "r+") as f:
             current = f.read()
-            print(" -> Current version = {} (from {})".format(current, self.version_file))
 
             f.seek(0)
             f.write(latest_version)
             f.truncate()
 
-            return current != latest_version
+            return current
 
     def commit_changes(self, latest_version):
-        message = "Update {} to version '{}'".format(self.name, latest_version)
+        message = "Update {} to version {}".format(self.name, latest_version)
 
-        subprocess.run(["git", "add", self.version_file])
-        subprocess.run(["git", "commit", "-m", message])
+        self.execute_git(["add", self.version_file])
+        self.execute_git(["commit", "-m", message])
 
-class NpmUpdater(Updater):
-    def __init__(self, name, version_file, npm_package):
+    def execute_git(self, args):
+        command = ["git"] + args
+        print("-> Executing {}".format(command))
+        subprocess.run(command)
+
+class NpmFetcher:
+
+    def __init__(self, npm_package):
         self.npm_package = npm_package;
-        super().__init__(name, version_file)
 
-    def update(self):
-        print("Updating {} via npm".format(self.name))
-
-        latest_version = self.fetch_from_registry()
-        super().update(latest_version)
-
-    def fetch_from_registry(self):
+    def fetch_latest(self):
         url = "https://registry.npmjs.org/{}".format(self.npm_package)
+        print("-> Fetching {}".format(url))
+
         res = requests.get(url)
         data = res.json()
         latest = data["dist-tags"]["latest"]
 
-        print(" -> Latest version = {} (from {})".format(latest, url))
         return latest
 
 def main():
     projects = [
-        NpmUpdater(
+        Updater(
             name="MeshCentral",
             version_file="meshcentral/VERSION",
-            npm_package="meshcentral",
+            version_fetcher=NpmFetcher("meshcentral"),
         ),
     ]
 
